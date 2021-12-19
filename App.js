a890f99e816e9743d4142d78604e555d92385414
SALT0601 <thfxm0601@gmail.com>
2021-12-19 19:53:09 +0900

    #1 Start
    앱 시작

---
+++ App.js
@@ -0,0 +1,154 @@
+
+import * as Location from "expo-location";
+
+import { Fontisto } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Dimensions,Text, View, ScrollView, ActivityIndicator, } from 'react-native';
+
+const {width : SCREEN_WIDTH} = Dimensions.get("window");
+const API_KEY = "1d853d1862a5f996c9ec6b0a8f92c7b3";
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
+
+
+export default function App() {
+  const[city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async() => {
+   const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted) {
+      setOk(false);
+    }
+    const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync(
+      {latitude, longitude}, 
+      {useGoogleMaps:false});
+      setCity(location[0].city);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`);
+      const json = await response.json();
+      setDays(json.daily);
+
+  };
+  useEffect(() => {
+    getWeather();
+
+  }, [])
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView horizontal
+       pagingEnabled
+       showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            
+            <View key={index} style={styles.day}>
+             
+             <Text style={styles.date} >{new Date(day.dt * 1000).toString().substring(0, 10)}</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="#0A3A64"
+                  style={{marginTop: 20}}
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EAAD5A',
+  },
+  city: {
+    flex: 1.2,
+    
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 46,
+    fontWeight: "900",
+    color: "#0A3A64",
+
+
+  },
+  weather: {
+    
+  },
+  day: {
+    width:  SCREEN_WIDTH,
+    
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+   
+  },
+  temp: {
+    marginTop: 20,
+    fontWeight: "600",
+    fontSize: 96,
+    color: "#0A3A64",
+    alignItems: "flex-start",
+    marginLeft: -8
+  },
+  description : {
+    marginTop: -10,
+    fontSize: 30,
+    color: "#0A3A64",
+    fontWeight: "500",
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    color: "#0A3A64",
+    fontWeight: "500",
+  },
+  date: {
+    fontSize: 20,
+    color: "#0A3A64",
+    fontWeight: "500",
+    alignItems: "center",
+
+  }
+
+})
